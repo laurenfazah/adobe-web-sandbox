8b00a7b1f8ea8215cd9e3b55378112156b51cae1
laurenfazah <laurenfazah@gmail.com>
2015-10-30 12:13:12 -0400

    image added dynamically through input

--- build/js/main.js
+++ build/js/main.js
@@ -1,3 +1,17 @@
 $(function(){
 
+    $('.imageArea').hide();
+
+
+    $('form[name="submitImgSrc"]').on('submit', function(e){
+        e.preventDefault();
+
+        var src = $('input[name="imgSrc"]').val();
+        console.log(src);
+
+        $('#editableimage').attr('src', src);
+
+        $('.imageArea').show();
+    });
+
 });
--- html/js/main.js
+++ html/js/main.js
@@ -1,1 +1,7 @@
-$(function() {});
+$(function() {
+    $(".imageArea").hide(), $('form[name="submitImgSrc"]').on("submit", function(e) {
+        e.preventDefault();
+        var src = $('input[name="imgSrc"]').val();
+        console.log(src), $("#editableimage").attr("src", src), $(".imageArea").show();
+    });
+});
